0013c52152cb9d18d19038ce17a8a0993f1fad6d
Adrian G <144031403+AP-BSU@users.noreply.github.com>
2023-09-18 12:38:29 +0400

    Exercise series - chapter 1

---
+++ Chapter 1- Getting Started with Python/Exercise C3 - Date and Time.py
@@ -0,0 +1,3 @@
+import datetime
+current_time = datetime.datetime.now()
+print ("Date and time:", current_time.strftime("%d-%m-%y %Hh:%Mm:%Ss"))
---
+++ Chapter 1- Getting Started with Python/Exercise C5 - Radius of a circle.py
@@ -0,0 +1,6 @@
+string1 = "Radius of a circle:"
+string2 =  "c = 6.3"
+string3 =  " R = 6.3/2 x π = 1.00268"
+string4 = " Radius is 1.00"
+combined_string = string1+ " " + string2 + "" + string3 + string4
+print (combined_string)
