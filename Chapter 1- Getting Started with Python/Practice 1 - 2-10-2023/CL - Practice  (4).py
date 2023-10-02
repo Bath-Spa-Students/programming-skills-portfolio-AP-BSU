33bdc9e67c71c167c05bcc810596b6021e40e495
Adrian G <144031403+AP-BSU@users.noreply.github.com>
2023-10-02 18:51:15 +0400

    Add files via upload

---
+++ Chapter 1- Getting Started with Python/Practice 1 - 2-10-2023/CL - Practice  (4).py
@@ -0,0 +1,4 @@
+base = float(input("Your value for length of the base: "))
+height = float(input("Your value for height of the triangle: "))
+area = (base * height) / 2
+print(f"The area of the triangle is: {area}")
---
+++ Chapter 1- Getting Started with Python/Practice 1 - 2-10-2023/CL - Practice  (5).py
@@ -0,0 +1,6 @@
+string1 = "This is the 1st string,"
+string2 =  "2nd string, "
+string3 =  "3rd string "
+string4 = "then the final 4th string "
+combined_string = string1+ " " + string2 + "" + string3 + "" + string4
+print (combined_string)
