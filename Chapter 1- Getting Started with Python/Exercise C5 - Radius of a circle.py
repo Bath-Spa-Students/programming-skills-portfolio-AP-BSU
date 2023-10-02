5e021a7130c327ad30d3d40d63824e05d32e8003
Adrian G <144031403+AP-BSU@users.noreply.github.com>
2023-10-02 18:30:42 +0400

    Update Exercise C5 - Radius of a circle.py

--- Chapter 1- Getting Started with Python/Exercise C5 - Radius of a circle.py
+++ Chapter 1- Getting Started with Python/Exercise C5 - Radius of a circle.py
@@ -1,6 +1,14 @@
-string1 = "Radius of a circle:"
-string2 =  "c = 6.3"
-string3 =  " R = 6.3/2 x π = 1.00268"
-string4 = " Radius is 1.00"
-combined_string = string1+ " " + string2 + "" + string3 + string4
-print (combined_string)
+import math
+def calculate_circle_area(radius):
+    area = math.pi * radius**2
+    return area
+def main():
+    radius = float(input("Please enter your radius value: "))
+    if radius < 0:
+        print("Value and radius cannot be negative.")
+    else:
+        area = calculate_circle_area(radius)
+        print(f"Area of the circle with radius {radius} is {area:.2f}")
+        print(f"The equation of the circle is: x^2 + y^2 = {radius**2:.2f}")
+if __name__ == "__main__":
+    main()
