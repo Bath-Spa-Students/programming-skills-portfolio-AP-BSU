4618646bdb9aa60162a1ca72511089f24c26b1c8
Adrian G <144031403+AP-BSU@users.noreply.github.com>
2023-10-05 12:38:42 +0400

    Add files via upload

---
+++ Chapter 2- Variables & Comments/Chapter 2 - Practice/Practice - Python prints series.py
@@ -0,0 +1,29 @@
+Name = input ("Enter Name: ")
+print (Name)
+
+Age = input ("Enter Age: ")
+print (Age)
+
+University = input ("Enter University: ")
+print (University)
+
+Location = input ("Enter Location: ")
+print (Location)
+
+Guardian = input ("Enter your parent or guardian name: ")
+print (Guardian)
+
+Nationality = input ("Enter your nationality: ")
+print (Nationality)
+
+Hobby = input ("Enter your hobby: ")
+print (Hobby)
+
+Skill = input ("What is your skill: ")
+print (Skill)
+
+Language = input ("Enter your language: ")
+print (Language)
+
+OS = input ("What is your operating system? ")
+print (OS)
