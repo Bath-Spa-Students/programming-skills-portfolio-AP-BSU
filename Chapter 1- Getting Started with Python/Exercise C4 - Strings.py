2e278c3f3a69bfa7a9ba2aaacaafaafe74040477
Adrian G <144031403+AP-BSU@users.noreply.github.com>
2023-10-23 12:45:22 +0400

    Delete Chapter 1- Getting Started with Python/Exercise C4 - Strings.py

--- Chapter 1- Getting Started with Python/Exercise C4 - Strings.py
+++
@@ -1,5 +0,0 @@
-string1 = "This is the first string,"
-string2 =  "second string,"
-string3 =  " Then the third and final string"
-combined_string = string1+ " " + string2 + "" + string3
-print (combined_string)
